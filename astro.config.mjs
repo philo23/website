6cf45bc028e5a49ed3419f808d1d52b637dc3d50
Philip Pryce <philip.pryce@me.com>
2026-02-15 17:28:56 +0000

    Adding MDX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig, envField, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
   integrations: [
     react(),
     sitemap(),
+    mdx(),
   ],
   vite: {
     plugins: [tailwindcss()],
